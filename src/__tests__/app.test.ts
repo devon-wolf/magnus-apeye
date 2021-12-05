c561d68c5ce9ec87c175c2bded5fd31db893ed7a
Devon Wolfkiel <devon.wolfkiel@gmail.com>
2021-12-05 09:13:50 -0800

    add basic tests for template request handling

--- src/__tests__/app.test.ts
+++ src/__tests__/app.test.ts
@@ -1,12 +1,21 @@
 import pool from '../lib/utils/pool';
 import setup from '../data/setup';
+import request from 'supertest';
+import app from '../lib/app';
 
 describe('app tests', () => {
   beforeEach(async () => {
     await setup(pool);
   });
 
-  it('is a placeholder test that will pass', () => {
-    expect(2 + 2).toEqual(4);
+  it('responds to a request', async () => {
+    const response = await request(app).get('/');
+    expect(response).toBeTruthy();
   });
+
+  it('responds to a nonexistent endpoint with a 404 error', async () => {
+    const response = await request(app).get('/not-an-implemented-endpoint');
+    expect(response.status).toBe(404);
+    expect(response.error).toBeTruthy();
+  })
 });
