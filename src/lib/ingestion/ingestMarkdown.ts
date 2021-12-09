ecef353033761edf97fb7a402850a256f226bb56
Devon Wolfkiel <devon.wolfkiel@gmail.com>
2021-12-08 19:58:48 -0800

    add function to read a directory of files

--- src/lib/ingestion/ingestMarkdown.ts
+++ src/lib/ingestion/ingestMarkdown.ts
@@ -2,12 +2,9 @@ import * as fs from 'fs/promises';
 
 const readRawFile = async (filename: string) => {
   try {
-    const data = await fs.readFile(
-      `${__dirname}/../../../assets/${filename}.md`,
-      {
-        encoding: 'utf-8',
-      }
-    );
+    const data = await fs.readFile(`${__dirname}/../../../assets/${filename}`, {
+      encoding: 'utf-8',
+    });
     return data;
   } catch (error) {
     console.error(error);
@@ -19,3 +16,19 @@ const readRawFile = async (filename: string) => {
 //   readRawFile('sample-episode')
 //      .then((result) => console.log(result))
 // );
+
+const readAllAssets = async () => {
+  try {
+    const dirFiles = await fs.readdir(`${__dirname}/../../../assets`);
+
+    const allFileContents = await Promise.all(
+      dirFiles.map((file) => readRawFile(file))
+    );
+
+    return allFileContents;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+console.log(readAllAssets().then((result) => console.log(result)));
