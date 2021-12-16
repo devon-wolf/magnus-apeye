066f72699e86fa89b57897ce9472f093f69ea644
Devon Wolfkiel <devon.wolfkiel@gmail.com>
2021-12-15 19:49:14 -0800

    add get request handlers and basic instructions

--- src/lib/controllers/episodes.ts
+++ src/lib/controllers/episodes.ts
@@ -2,9 +2,18 @@ import { Router } from 'express';
 import Episode from '../models/Episode';
 
 const episodesController = Router()
-  .get('/', (req, res, next) => {
+  .get('/', async (req, res, next) => {
     try {
-      res.send('response');
+      const episodes = await Episode.getAll();
+      res.send(episodes);
+    } catch (error) {
+      next(error);
+    }
+  })
+  .get('/:episodeNumber', async (req, res, next) => {
+    try {
+      const episode = await Episode.getByEpisodeNumber(Number(req.params.episodeNumber));
+      res.send(episode);
     } catch (error) {
       next(error);
     }
