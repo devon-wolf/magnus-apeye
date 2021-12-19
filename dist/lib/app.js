681ffc30cf0b4079c3b30a3eaba8345c1cb8f955
Devon Wolfkiel <devon.wolfkiel@gmail.com>
2021-12-19 11:32:38 -0800

    add hacky db setup endpoint

--- dist/lib/app.js
+++ dist/lib/app.js
@@ -5,11 +5,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var episodes_1 = __importDefault(require("./controllers/episodes"));
+var pool_1 = __importDefault(require("./database/pool"));
+var setup_1 = __importDefault(require("./database/setup"));
 var error_handling_1 = require("./middleware/error-handling");
 var app = (0, express_1.default)();
 app.use(express_1.default.static('public'));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
+// TODO replace this, should work for now
+app.post('/setup-db', function (req, res, next) {
+    return (0, setup_1.default)(pool_1.default)
+        .then(function () { return res.send('Database setup complete.'); })
+        .catch(next);
+});
 app.use('/episodes', episodes_1.default);
 app.use(error_handling_1.handleNotFound);
 app.use(error_handling_1.handleError);
--- src/lib/app.ts
+++ src/lib/app.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import episodesController from './controllers/episodes';
+import pool from './database/pool';
+import setup from './database/setup';
 import { handleError, handleNotFound } from './middleware/error-handling';
 
 const app = express();
@@ -8,6 +10,12 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// TODO replace this, should work for now
+app.post('/setup-db', (req, res, next) => {
+  return setup(pool)
+    .then(() => res.send('Database setup complete.'))
+    .catch(next);
+});
 app.use('/episodes', episodesController);
 
 app.use(handleNotFound);
