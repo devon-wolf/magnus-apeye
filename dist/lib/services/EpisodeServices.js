04352d60331684ed12714f428a029ff5275022ce
Devon Wolfkiel <git@devonwolf.simplelogin.com>
2022-12-23 12:17:54 -0800

    remove dist folder

--- dist/lib/services/EpisodeServices.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var EpisodeServices = /** @class */ (function () {
-    function EpisodeServices() {
-    }
-    return EpisodeServices;
-}());
-exports.default = EpisodeServices;
